f2c890fc057e5c108252786d6d7d4614f0fe7a11
annseonghyeoon <ash031011@gmail.com>
2023-10-20 14:17:11 +0900

    sample

--- WebService/apply.py
+++ WebService/apply.py
@@ -2,15 +2,16 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.text('Fixed width text')
-st.markdown('_Markdown_') # see #*
-st.caption('Balloons. Hundreds of them...')
-st.latex(r''' e^{i\pi} + 1 = 0 ''')
-st.write('Most objects') # df, err, func, keras!
-st.write(['st', 'is <', 3]) # see *
-st.title('My title')
-st.header('My header')
-st.subheader('My sub')
-st.code('for i in range(8): foo()')
+def example_link_and_colors():
+    mdlit(
+        """Tired from [default links](https://extras.streamlit.app)?
+    Me too! Discover Markdownlit's `@()` operator. Just insert a link and it
+    will figure a nice icon and label for you!
+    Example: @(https://extras.streamlit.app)... better, right? You can
+    also @(🍐)(manually set the label if you want)(https://extras.streamlit.app)
+    btw, and play with a [red]beautiful[/red] [blue]set[/blue] [orange]of[/orange]
+    [violet]colors[/violet]. Another perk is those beautiful arrows -> <-
+    """
+    )
 
-# * optional kwarg unsafe_allow_html = True
+
