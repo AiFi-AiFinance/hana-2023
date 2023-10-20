da9250d307326282143b87101596863b5336f1cc
annseonghyeoon <ash031011@gmail.com>
2023-10-20 13:59:09 +0900

    sample1

--- WebService/apply.py
+++ WebService/apply.py
@@ -2,14 +2,15 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.write(""" 
-양승연
-""")
+st.text('Fixed width text')
+st.markdown('_Markdown_') # see #*
+st.caption('Balloons. Hundreds of them...')
+st.latex(r''' e^{i\pi} + 1 = 0 ''')
+st.write('Most objects') # df, err, func, keras!
+st.write(['st', 'is <', 3]) # see *
+st.title('My title')
+st.header('My header')
+st.subheader('My sub')
+st.code('for i in range(8): foo()')
 
-#df = pd.read_csv("output.csv")
-x = st.slider('x') # 👈 this is a widget
-st.write(x, 'squared is', x * x)
-st.write("안성현")
-
-# You can access the value at any point with:
-st.session_state.name
+# * optional kwarg unsafe_allow_html = True
