881d444c9555c48d45202a2db2e7a0d26a2f4864
annseonghyeoon <ash031011@gmail.com>
2023-10-20 14:20:27 +0900

    sample

--- WebService/sample_web.py
+++ WebService/sample_web.py
@@ -1,12 +1,21 @@
-def example_link_and_colors():
-    mdlit(
-        """Tired from [default links](https://extras.streamlit.app)?
-    Me too! Discover Markdownlit's `@()` operator. Just insert a link and it
-    will figure a nice icon and label for you!
-    Example: @(https://extras.streamlit.app)... better, right? You can
-    also @(🍐)(manually set the label if you want)(https://extras.streamlit.app)
-    btw, and play with a [red]beautiful[/red] [blue]set[/blue] [orange]of[/orange]
-    [violet]colors[/violet]. Another perk is those beautiful arrows -> <-
-    """
-    )
+import streamlit as st
+import numpy as np
+import pandas as pd
 
+st.button('Hit me')
+st.data_editor('Edit data', data)
+st.checkbox('Check me out')
+st.radio('Pick one:', ['nose','ear'])
+st.selectbox('Select', [1,2,3])
+st.multiselect('Multiselect', [1,2,3])
+st.slider('Slide me', min_value=0, max_value=10)
+st.select_slider('Slide to select', options=[1,'2'])
+st.text_input('Enter some text')
+st.number_input('Enter a number')
+st.text_area('Area for textual entry')
+st.date_input('Date input')
+st.time_input('Time entry')
+st.file_uploader('File uploader')
+st.download_button('On the dl', data)
+st.camera_input("一二三,茄子!")
+st.color_picker('Pick a color')
