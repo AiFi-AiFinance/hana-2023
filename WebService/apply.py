9c679b387b58c391c0f4a89120c24370767d41b4
annseonghyeoon <ash031011@gmail.com>
2023-10-20 13:44:26 +0900

    ann

--- WebService/apply.py
+++ WebService/apply.py
@@ -3,14 +3,13 @@ import numpy as np
 import pandas as pd
 
 st.write(""" 
-#광고 신청
+양승연
 """)
 
 #df = pd.read_csv("output.csv")
 x = st.slider('x') # 👈 this is a widget
 st.write(x, 'squared is', x * x)
-st.write(df)
-st.text_input("Your name", key="name")
+st.write("안성현")
 
 # You can access the value at any point with:
 st.session_state.name
