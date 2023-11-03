46a256805b071eeb72c44220e08874bfcb151493
annseonghyeoon <ash031011@gmail.com>
2023-11-03 14:54:58 +0900

    ann

--- WebService/apply2.py
+++ WebService/apply2.py
@@ -19,7 +19,7 @@ from dateutil import parser
 
 #제목
 col1,empty2,col2 = st.columns([1, 0.3, 8.7])
-low1,enmty,low2 = st.low([1, 0.3, 5])
+low1,enmty2,low2 = st.low([1, 0.5, 5])
 
 with col2, low1 :
     st.title("기업 광고 신청페이지\n")
@@ -30,7 +30,7 @@ with col2, low1 :
 col1,empty2,col2 = st.columns([1, 0.03, 1])
 
 #(1) 업체명 이메일 전화번호 비밀번호
-with col1 :
+with col1, low2 :
     st.subheader("기업 정보")
     store_name = st.text_input('업체명을 입력해주세요.', value="")
     email = st.text_input('이메일을 입력해주세요.', value="")
