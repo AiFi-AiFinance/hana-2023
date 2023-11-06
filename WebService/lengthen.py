9aa95bccc391fb6efe7b42c7e964fe267d949e12
annseonghyeon <148517593+annseonghyeon@users.noreply.github.com>
2023-11-06 18:15:48 +0900

    Update lengthen.py

--- WebService/lengthen.py
+++ WebService/lengthen.py
@@ -14,6 +14,19 @@ import re
 from dateutil import parser
 
 
+col1,empty2,col2 = st.columns([1, 0.3, 8.7])
+
+
+
+
+
+with col2 :
+    st.title("기업 광고 연장페이지\n")
+
+
+
+
+
 #(1) 연장기간 (yy.mm.dd ~ yy.mm.dd)
 with col2 :
     start_date = st.date_input('광고 종료일을 선택해주세요.')
@@ -26,5 +39,3 @@ deadline = parser.parse(str(deadline))
 dur = (deadline - start_date).days
 if (dur <= 0): st.error("광고기간 입력 오류입니다. 입력한 광고 종료일과 연장일을 다시 한 번 확인해주세요.")
 df.loc['광고 기간'] = dur
-
-   
