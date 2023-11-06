7e8a5adaa12b2416d45dff3fd5bfa49c8cda6304
annseonghyeon <148517593+annseonghyeon@users.noreply.github.com>
2023-11-06 18:50:38 +0900

    Update lengthen.py

--- WebService/lengthen.py
+++ WebService/lengthen.py
@@ -27,7 +27,12 @@ with col2 :
 
 col1,empty2,col2 = st.columns([1, 0.03, 1])
 
-#(1) 연장기간 (yy.mm.dd ~ yy.mm.dd)
+#(1) 기업 정보
+with col1 :
+    st.subheader("기업 정보")
+    store_name = st.text_input('업체명을 입력해주세요.', value="")
+
+#(2) 연장기간 (yy.mm.dd ~ yy.mm.dd)
 with col2 :
     st.subheader("광고 정보")
 with col2 :
@@ -35,14 +40,11 @@ with col2 :
 with col2 :
     deadline = st.date_input('광고 연장일을 선택해주세요.')
     
- #(2) 광고 기간 계산
+ #(3) 광고 기간 계산
 start_date = parser.parse(str(start_date))
 deadline = parser.parse(str(deadline))
 dur = (deadline - start_date).days
 if (dur <= 0): st.error("광고기간 입력 오류입니다. 입력한 광고 종료일과 연장일을 다시 한 번 확인해주세요.")
 df.loc['광고 기간'] = dur
 
- #(3) 기업 정보
-with col1 :
-    st.subheader("기업 정보")
-    store_name = st.text_input('업체명을 입력해주세요.', value="")
+ 
