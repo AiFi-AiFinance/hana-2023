48ff8adf187777e464ca6cb4f16d9a0bee00ec78
Soyeon Kwon <soyeon@Soyeonui-MacBookPro.local>
2023-12-01 01:12:59 +0900

    광고신청

--- WebService/pages/3_광고신청.py
+++ WebService/pages/3_광고신청.py
@@ -21,57 +21,53 @@ with st.sidebar:
 store_code = 1
 ad_code = 1
 
-from dateutil import parser
-
-
-col1,empty2,col2 = st.columns([1, 0.03, 1])
-
-#(1) 업체명 이메일 전화번호 비밀번호
-with col1 :
-  st.subheader("기업 정보")
-  store_name = st.text_input('업체명을 입력해 주세요.', value="")
-  email = st.text_input('이메일을 입력해 주세요.', value="")
-  phone = st.text_input('전화번호를 입력해 주세요.', value="")
-  passwd = st.text_input('비밀번호를 입력해 주세요.', value="", type='password')
+# 광고 신청
+st.subheader("광고신청")
+col1,col2 = st.columns(2)
+with col1:
+  title = st.text_input("광고 제목",placeholder="광고 제목을 입력해주세요.")
+  contents = st.text_area("광고 내용",placeholder="광고 내용을 입력해주세요.")
+  category = ['카테고리 선택','문화/생활', '여행/해외', '쇼핑/무이자', '정기결제', '할인/캐시백', '응모/경품', 'QR 결제']
+  selected_category = st.selectbox("카테고리",options=category,placeholder="카테고리를 선택해주세요.")
     
-#(2) 광고
-with col2 :
-  st.subheader("광고 정보")
-  title = st.text_input('광고 제목을 입력해 주세요.', value="")
-  contents = st.text_area('광고 내용을 입력해 주세요.', value="")
-
-  #(2) 카테고리 summary
-  summary = ['선택', '문화/생활', '여행/해외', '쇼핑/무이자', '정기결제', '할인/캐시백', '응모/경품', 'QR 결제']
-  selected_summary = st.selectbox('카테고리',summary)
-
-  #(3) 이미지 넣기
-  image = st.file_uploader('이미지를 업로드 하세요.', type=['png', 'jpg', 'jpeg'])
-    
-  #(4) 광고기간 (yy.mm.dd ~ yy.mm.dd)
-  start_date = st.date_input('광고 시작일을 선택해 주세요.')
-  deadline = st.date_input('광고 종료일을 선택해 주세요.')
-    
-  #(5) 광고 기간 계산
+with col2:
+  keyword = st.text_input("키워드",placeholder="\' , \'로 구분해서 입력해주세요.")
+  image = st.file_uploader("이미지", type=['png', 'jpg', 'jpeg'])
+  start_date = st.date_input("광고 시작일")
+  deadline = st.date_input("광고 종료일")
+  
+  from dateutil import parser
   start_date = parser.parse(str(start_date))
   deadline = parser.parse(str(deadline))
   dur = (deadline - start_date).days
   if (dur <= 0): st.error("광고 기간 입력 오류입니다. 입력한 광고 시작일과 종료일을 다시 한 번 확인해 주세요.")
 
-# 데이터 베이스 파일에서 insert
-def get_enterprise():
-  if store_code and store_name and email and phone and passwd:
-    return store_code, store_name, email, phone, passwd
+if st.button("광고신청",type="primary"):
+  if title == "":  
+    st.warning("잘못된 광고 제목 입니다.")
+  elif contents == "":  
+    st.warning("잘못된 광고 내용 입니다.")
+  elif selected_category == "":  
+    st.warning("잘못된 카테고리 입니다.")
+  elif keyword == "":  
+    st.warning("잘못된 키워드 입니다.")
   else:
-    return -1
+    # insert_ad(title,contents,category,keyword,image,start_date,deadline)
+    st.success("광고신청이 완료되었습니다.")
+    
+    
+# 데이터 베이스 파일에서 insert
+# def get_enterprise():
+#   if store_code and store_name and email and phone and passwd:
+#     return store_code, store_name, email, phone, passwd
+#   else:
+#     return -1
 
 # GPT 파일에서 GPT_ad 받아오기
 # 타입 분류 머신러닝 파일에서 타입 받아오기
 # 그 다음에 데이터 베이스 파일에서 insert
-def get_advertisement():
-  if ad_code and store_code and title and contents and summary and image and start_date and deadline:
-    return ad_code, store_code, title, contents, summary, image, start_date, deadline
-  else:
-    return -1
-   
-if st.button("광고 신청"):
-    st.success("광고 신청이 성공적으로 완료되었습니다.")
+# def get_advertisement():
+#   if ad_code and store_code and title and contents and summary and image and start_date and deadline:
+#     return ad_code, store_code, title, contents, summary, image, start_date, deadline
+#   else:
+#     return -1
